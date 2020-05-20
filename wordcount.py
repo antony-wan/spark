54531b902be0e10a7ce505539073ca8083eb9b46
AntonyWAN <antonyw@kth.se>
2020-05-20 10:54:49 +0200

    add comments

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,20 @@
 import sys
 from pyspark import SparkContext
 
-sc = SparkContext.getOrCreate();
+sc = SparkContext();
+#Instancie un objet de type SparkContext qui gère les propriétés globales de l'application
 
-lines = sc.textFile('text.txt')
-word_counts = lines.flatMap(lambda line: line.split(' ')) \
-                   .map(lambda word: (word, 1)) \
-                   .reduceByKey(lambda count1, count2: count1 + count2) \
-                   .collect()
+lines = sc.textFile(sys.argv[1])
+#sys.argv[1] = paramètre passé en ligne de cmd
+#lines est de type RDD
+
+word = lines.flatMap(lambda line: line.split(' '))
+#Comme la fonction map sauf qu'il est possible d'associer plusieurs sorties à une entree
+words_with_1 = word.map(lambda word: (word, 1))
+word_counts = words_with_1.reduceByKey(lambda count1, count2: count1 + count2)
+result = word_counts.collect()
+#result est une liste de tuples (clé, valeur)
+#La methode collect() est une action
 
 for (word, count) in word_counts:
     print(word, count)
